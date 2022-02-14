ae254b6830bcc8183e4c52a5864d5d71b43158a7
ESzabika26 <eklicsszabi@gmail.com>
2022-02-14 10:28:33 +0100

    10.feladat

--- 2022_02_14_Iskola/Form1.cs
+++ 2022_02_14_Iskola/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _2022_02_14_Iskola
 {
@@ -19,7 +20,8 @@ namespace _2022_02_14_Iskola
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            File.ReadAllLines("nevek.txt").ToList()
+                .ForEach(sor => listBox1.Items.Add(sor));
         }
     }
 }
